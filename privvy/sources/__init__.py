0381e85a893b22d479d08d5e995750cca8737d4a
Chris Lee <sihrc.c.lee@gmail.com>
2017-04-10 17:38:43 -0400

    FIX: warning message for environment mapping missing

--- privvy/sources/__init__.py
+++ privvy/sources/__init__.py
@@ -3,5 +3,5 @@ import os
 def resolve_env_var(env, mapping):
     env_var = mapping.get(env, env)
     if env_var not in os.environ:
-        print("Warning: {env} environment variable is missing".format(env_var))
+        print("Warning: {env} environment variable is missing".format(env=env_var))
     return os.getenv(env_var)
